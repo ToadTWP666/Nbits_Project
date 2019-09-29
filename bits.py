a61b92b134b472ebb59d18503520b939d6cdf75f
ToadTWP666 <durhamrick123@yahoo.com>
2019-09-29 11:08:29 -0400

    Update bits.py

--- bits.py
+++ bits.py
@@ -2,6 +2,7 @@
 #Desc: This program is designed so that the user can input a number(n),
 # and the output will show all the unique ways to organize n-bits
 #Date: 2/07/2019
+#Test comment Dad
 
 import itertools
 def nbits(n):
